0ace3eeab236fefc9871a78fb3b3a9a7cac70672
Luke <lukemckinstry@gmail.com>
2018-05-09 19:01:11 -0400

    init commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, url_for, redirect, request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect(url_for('welcome'))
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+#POST route NGP VAN
+
+@app.route('/postvan')
+def post_van():
+    return 'Posting to NGP VAN'
+
+#GET route Volunteer Match
+
+@app.route('/getvm')
+def get_vm():
+    return 'Getting from Volunteer Match'
