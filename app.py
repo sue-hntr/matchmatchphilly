017551396ed853c0a50bcca0062fa884f9e12c7b
Luke <lukemckinstry@gmail.com>
2018-05-21 22:20:08 -0400

    add results.html page

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, url_for, redirect, request
 from flask import render_template
 
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,14 +20,21 @@ def login():
 def welcome():
     return render_template('welcome.html')
 
+@app.route('/results')
+def results():
+    return render_template('results.html')
+
 #POST route NGP VAN
 
 @app.route('/postvan')
 def post_van():
-    return 'Posting to NGP VAN'
+	return 'Post to NGP VAN'
 
 #GET route Volunteer Match
 
 @app.route('/getvm')
 def get_vm():
     return 'Getting from Volunteer Match'
+
+if __name__ == "__main__":
+	app.run()
